df9f1965fb7a19649ffc5d16f7b09055514e1205
julienbrs <julien.bourseau@gmail.com>
2024-05-16 11:15:11 +0200

    pushy push

--- src/app/form/FormPage.tsx
+++ src/app/form/FormPage.tsx
@@ -151,17 +151,33 @@ const FormPage = () => {
         setError('Please select a region');
         return;
       }
+
+      console.log("ha", ha);
+      console.log("treeCrownCover", treeCrownCover);
+      console.log("shrubCrownCover", shrubCrownCover);
+      console.log("shrubArea", shrubArea);
+      console.log("projectTreeCrownCover", projectTreeCrownCover);
+      console.log("projectShrubCrownCover", projectShrubCrownCover);
+      console.log("projectShrubArea", projectShrubArea);
+      console.log("treeRootShoot", treeRootShoot);
+      console.log("shrubRootShoot", shrubRootShoot);
+      console.log("shrubBiomass", shrubBiomass);
+
       // Calculating CTREE_BASELINE
       const CTREE_BASELINE = (44 / 12) * CFTREE * bFOREST * (1 + treeRootShoot) * treeCrownCover * ha;
       // Calculating CSHRUB_t
       const CSHRUB_t = (44 / 12) * CFS * (1 + shrubRootShoot) * shrubArea * shrubBiomass * bFOREST * shrubCrownCover;
       // Calculating CTREE_PROJECT
-      const CTREE_PROJECT = (44 / 12) * CFTREE * bFOREST * (1 + shrubRootShoot) * projectTreeCrownCover * ha;
+      const CTREE_PROJECT = (44 / 12) * CFTREE * bFOREST * (1 + treeRootShoot) * projectTreeCrownCover * ha;
       // Calculating CSHRUB_PROJECT
       const CSHRUB_PROJECT = (44 / 12) * CFS * (1 + shrubRootShoot) * projectShrubArea * shrubBiomass * bFOREST * projectShrubCrownCover;
 
+      console.log("CTREE_BASELINE", CTREE_BASELINE);
+      console.log("CSHRUB_t", CSHRUB_t);
+      console.log("CTREE_PROJECT", CTREE_PROJECT);
+      console.log("CSHRUB_PROJECT", CSHRUB_PROJECT);
       // Final CO2 estimated value
-      const finalCO2Estimated = CTREE_PROJECT + CSHRUB_PROJECT -CTREE_BASELINE - CSHRUB_t;
+      const finalCO2Estimated = CTREE_PROJECT + CSHRUB_PROJECT - CTREE_BASELINE - CSHRUB_t;
 
       setResult(finalCO2Estimated);
       setShowResult(true);
@@ -235,91 +251,104 @@ const FormPage = () => {
       </header>
 
 
-        {!showResult ? (
-          <div className={`grid ${complementaryInfo(currentQuestion) ? 'grid-cols-5' : 'grid-cols-5'} bg-white p-12 rounded w-screen relative overflow-hidden border-transparent h-64`}>
-            <div className={`inset-0 ${complementaryInfo(currentQuestion) ? 'col-span-3' : 'col-span-5'} ${animating ? 'slide-down' : 'slide-active'}`}>
-              <h2 className="text-xl font-semibold mb-2 text-gray-900">{questions[currentQuestion].question}</h2>
-              {/* <p className="text-gray-500 mb-4">Put zero if you don't have it</p> */}
-              {questions[currentQuestion].type === 'select' ? (
-                <select
-                  value={answers[currentQuestion]}
-                  onChange={handleChange}
-                  className="border-b-2 border-gray-300 p-2 mb-4 w-full focus:outline-none focus:border-blue-500 text-gray-800"
-                >
-                  <option value="">Select your region</option>
-                  {questions[currentQuestion].options?.map((option) => (
-                    <option key={option} value={option}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
-              ) : questions[currentQuestion].type === 'information' ? (
-                <div>
-                  <p className="text-gray-800 mb-4">{questions[currentQuestion].info}</p>
-                  <button
-                    onClick={handleNext}
-                    className="text-blue-500 flex items-center"
-                  >
-                    <span className="mr-2">Next</span> &darr;
-                  </button>
-                </div>
-              ) : (
-                <input
-                  type={questions[currentQuestion].type}
-                  value={answers[currentQuestion]}
-                  onChange={handleChange}
-                  className="border-b-2 border-gray-300 p-2 mb-4 w-full focus:outline-none focus:border-blue-500 text-gray-800"
-                  placeholder="Type your answer here..."
-                />
-              )}
-              {error && <p className="text-red-500 mb-4">{error}</p>}
-              <div className="flex justify-between items-center">
+      {!showResult ? (
+        <div className={`grid ${complementaryInfo(currentQuestion) ? 'grid-cols-5' : 'grid-cols-5'} bg-white p-12 rounded w-screen relative overflow-hidden border-transparent h-64`}>
+          <div className={`inset-0 ${complementaryInfo(currentQuestion) ? 'col-span-3' : 'col-span-5'} ${animating ? 'slide-down' : 'slide-active'}`}>
+            <h2 className="text-xl font-semibold mb-2 text-gray-900">{questions[currentQuestion].question}</h2>
+            {/* <p className="text-gray-500 mb-4">Put zero if you don't have it</p> */}
+            {questions[currentQuestion].type === 'select' ? (
+              <select
+                value={answers[currentQuestion]}
+                onChange={handleChange}
+                className="border-b-2 border-gray-300 p-2 mb-4 w-full focus:outline-none focus:border-blue-500 text-gray-800"
+              >
+                <option value="">Select your region</option>
+                {questions[currentQuestion].options?.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            ) : questions[currentQuestion].type === 'information' ? (
+              <div>
+                <p className="text-gray-800 mb-4">{questions[currentQuestion].info}</p>
                 <button
-                  onClick={handlePrevious}
-                  disabled={currentQuestion === 0}
-                  className="text-blue-500 disabled:text-gray-300 flex items-center"
+                  onClick={handleNext}
+                  className="text-blue-500 flex items-center"
                 >
-                  &uarr; <span className="ml-2">Previous</span>
+                  <span className="mr-2">Next</span> &darr;
                 </button>
-                {!isLastQuestion && <span className="text-gray-500">press Enter</span>}
-                {isLastQuestion ? (
-                  <button
-                    onClick={handleCalculate}
-                    className="text-2xl font-bold text-blue-500"
-                  >
-                    OK
-                  </button>
-                ) : (
-                  <button
-                    onClick={handleNext}
-                    className="text-blue-500 flex items-center"
-                  >
-                    <span className="mr-2">Next</span> &darr;
-                  </button>
-                )}
               </div>
+            ) : (
+              <input
+                type={questions[currentQuestion].type}
+                value={answers[currentQuestion]}
+                onChange={handleChange}
+                className="border-b-2 border-gray-300 p-2 mb-4 w-full focus:outline-none focus:border-blue-500 text-gray-800"
+                placeholder="Type your answer here..."
+              />
+            )}
+            {error && <p className="text-red-500 mb-4">{error}</p>}
+            <div className="flex justify-between items-center">
+              <button
+                onClick={handlePrevious}
+                disabled={currentQuestion === 0}
+                className="text-blue-500 disabled:text-gray-300 flex items-center"
+              >
+                &uarr; <span className="ml-2">Previous</span>
+              </button>
+              {!isLastQuestion && <span className="text-gray-500">press Enter</span>}
+              {isLastQuestion ? (
+                <button
+                  onClick={handleCalculate}
+                  className="text-2xl font-bold text-blue-500"
+                >
+                  OK
+                </button>
+              ) : (
+                <button
+                  onClick={handleNext}
+                  className="text-blue-500 flex items-center"
+                >
+                  <span className="mr-2">Next</span> &darr;
+                </button>
+              )}
             </div>
-            {/* Complementary information div */}
-            <div className={`inset-0 grid grid-flow-col col-span-2 slide-down justify-center content-center ml-12 ${complementaryInfo(currentQuestion) ? 'visible' : 'invisible'}`}>
-              {complementaryInfo(currentQuestion)}
-            </div>
+          </div>
+          {/* Complementary information div */}
+          <div className={`inset-0 grid grid-flow-col col-span-2 slide-down justify-center content-center ml-12 ${complementaryInfo(currentQuestion) ? 'visible' : 'invisible'}`}>
+            {complementaryInfo(currentQuestion)}
+          </div>
+        </div>
+
+      ) : (
+        <div className="text-center">
+          <div className="bg-white p-8 rounded w-full max-w-xl sm:max-w-2xl relative overflow-hidden border-transparent h-auto flex flex-col items-center justify-center">
+            <h2 className="text-4xl font-bold text-green-500 animate-bounce mb-4">
+              Result: {formatNumber(result)} tons of CO2
+            </h2>
+            <h2 className="text-2xl font-semibold text-gray-800">
+              {result > 0 ? 'Great job! 🎉' : 'Please fill in all the fields'}
+            </h2>
+            {result > 0 && (
+              <h3 className="text-xl text-gray-600 mt-4">
+                Here is your proof of computation:
+                <a href="https://sepolia.voyager.online/tx/0x3e774a9ce6afb55fc47c9390776a681f6674162b15cfb9bd6d062df01c823ee"
+                  className="text-blue-500 hover:text-blue-700 ml-2">
+                  View Transaction
+                </a>
+              </h3>
+            )}
           </div>
 
-        ) : (
-          <div className="text-center">
-            <div className="bg-white p-8 rounded w-full max-w-xl sm:max-w-2xl relative overflow-hidden border-transparent h-64 flex items-center justify-center">
-              <h2 className="text-4xl font-bold text-green-500 animate-bounce">
-                Result: {formatNumber(result)} tons of CO2
-              </h2>
-            </div>
-            <button
-              onClick={handleRedo}
-              className="mt-8 bg-blue-500 text-white px-4 py-2 rounded"
-            >
-              Redo Form
-            </button>
-          </div>
-        )}
+          <button
+            onClick={handleRedo}
+            className="mt-8 bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            Redo Form
+          </button>
+        </div>
+      )}
     </div>
   );
 };
